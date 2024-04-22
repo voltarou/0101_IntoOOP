a95a96552631c3011b03db4f07f7abaf93bc4adb
voltarou <bagusadnan04@gmail.com>
2024-04-22 15:01:31 +0800

    void input

--- ggh/ggh.cpp
+++ ggh/ggh.cpp
@@ -7,4 +7,11 @@ private:
 	float panjang, Lebar;
 public:
 	float Luas;
+
+	void input() {
+		cout << "Masukan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukan Lebarnya = ";
+		cin >> Lebar;
+	}
 };
