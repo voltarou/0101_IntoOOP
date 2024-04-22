af3493c9726c947c620184cb2749a2302629b051
voltarou <bagusadnan04@gmail.com>
2024-04-22 14:30:16 +0800

    void output

--- yoii/yoii.cpp
+++ yoii/yoii.cpp
@@ -6,4 +6,10 @@ public:
 	string nama;
 	int umur;
 	string jurusan;
+
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan" << jurusan << endl;
+	}
 };
