dad705e25de62c9e3d728cf3d7e723c21802491b
voltarou <bagusadnan04@gmail.com>
2024-04-22 14:41:41 +0800

    void output2

--- yoii/yoii.cpp
+++ yoii/yoii.cpp
@@ -28,4 +28,10 @@ public :
 		cout << "SKS: ";
 		cin >> sks;
 	}
+
+	void output() {
+		cout << "Kode MK: " << kodeMk << endl;
+		cout << "Nama MK: " << namaMk << endl;
+		cout << "SKS: " << sks << endl;
+	}
 };
