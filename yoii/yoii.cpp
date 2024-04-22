0e51cbffc8e2d3e2b9a94c730af732297e22440a
voltarou <bagusadnan04@gmail.com>
2024-04-22 14:37:54 +0800

    void input

--- yoii/yoii.cpp
+++ yoii/yoii.cpp
@@ -20,4 +20,12 @@ private:
 	string namaMk;
 	int sks;
 public :
+	void input() {
+		cout << "Kode MK: ";
+		cin >> kodeMk;
+		cout << "Nama MK: ";
+		cin >> namaMk;
+		cout << "SKS: ";
+		cin >> sks;
+	}
 };
